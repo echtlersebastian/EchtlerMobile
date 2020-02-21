bf4346ec80d5dcc135dfd24fb605f58142422574
alexander.mosz <alexander.mosz@googlemail.com>
2020-02-21 20:15:22 +0100

    format

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -15,18 +15,17 @@ return(
 
         <div className="row text-center">
         {
-                    serviceLinks && serviceLinks.map(({ title, caption }) =>
+                    serviceLinks && serviceLinks.map(({ title, caption }, index) =>
 
 
-          <div className="col-md-4">
+          <div className="col-md-4" key={index}> 
             <span className="fa-stack fa-4x">
               <i className="fa fa-circle fa-stack-2x text-primary"></i>
               <i className="fa fa-lock fa-stack-1x fa-inverse"></i>
             </span>
             <h4 className="service-heading">{ title }</h4>
             <p className="text-muted">{ caption }</p>
-          </div>
-                    )
+          </div>)
         }
 
         </div>
