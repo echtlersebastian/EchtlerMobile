1b2c58f5ec8242d79c4b233f7d034a4834f2c294
Paul Stempel <stempel.paul@googlemail.com>
2020-06-11 19:49:45 +0200

    package updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,8 @@ return(
 
     <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
     <div className="container">
-      <a className="navbar-brand js-scroll-trigger" href="#page-top"></a>
+      <a className="navbar-brand js-scroll-trigger" href="#page-top">Echtler Mobile</a>
+      
       <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i className="fa fa-bars"></i>
